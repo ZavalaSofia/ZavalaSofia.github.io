b56f1269e8c956a856df3896aed9cdd385fbcdb0
Maria Sofia Zavala <125761586+ZavalaSofia@users.noreply.github.com>
2024-10-05 22:38:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #f3cd44 ">Desarrollo sitios web y tambien trabajo un poco en SolidWorks.</span>')
+  .typeString('<span style="color: #f3cd44 ">Divulgación de contnido para agricultores</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
