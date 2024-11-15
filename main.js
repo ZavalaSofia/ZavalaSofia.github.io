1899674a458835fa71090aa752f3a46fa84ebb7b
Maria Sofia Zavala <125761586+ZavalaSofia@users.noreply.github.com>
2024-11-15 16:39:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #f3cd44 ">Pagina de divulgación de contenido para agricultores</span>')
+  .typeString('<span style="color: #f3cd44 ">Divulgación de contnido para agricultores</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
